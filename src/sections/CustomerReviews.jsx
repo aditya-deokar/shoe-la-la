258b7f10b7eb1b014a55011d90533ad75b2365c7
adiya-deokar <adiyadeokar80@gmail.com>
2024-10-08 12:28:07 +0530

    added review Component & subscribe components

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,8 +1,28 @@
 
-
+import { reviews } from "../contants"
+import ReviewCard from "../components/ReviewCard"
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container ">
+      <h3 className="font-palanquin text-center text-4xl font-bold "> 
+        What Our <span className="text-coral-red">Customers </span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center ">hear genuine stories Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+      <div className="mt-24 flex justify-evenly items-center max-lg:flex-col gap-14  ">
+        {
+          reviews.map((item,index)=>(
+            <ReviewCard 
+              key={item.customerName}
+              imgURL={item.imgURL}
+
+             customerName={item.customerName}
+             rating={item.rating}
+             feedback={item.feedback}
+             />
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
